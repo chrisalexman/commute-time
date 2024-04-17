883563e4ad631238f1085f9c3afe24409fb3ff45
Christopher Alexman <christopheralexman@gmail.com>
2024-04-16 18:01:26 -0700

    Add commute time to a function

--- main.py
+++ main.py
@@ -13,15 +13,21 @@ from datetime import datetime
 
 gmaps = googlemaps.Client(key=credentials.api_key)
 
-home = locations.origin
-work = locations.destination
+# get commute time with traffic, store in json file
+def get_commute_time():
+    home = locations.origin
+    work = locations.destination
 
-now = datetime.now()
-commute_time = gmaps.distance_matrix(home,
-                                     work,
-                                     mode='driving',
-                                     avoid='tolls',
-                                     departure_time=now)
+    now = datetime.now()
+    commute_time = gmaps.distance_matrix(home,
+                                        work,
+                                        mode='driving',
+                                        avoid='tolls',
+                                        departure_time=now)
 
-with open('commute_time.json', 'w') as file:
-    json.dump(commute_time, file, indent=4)
+    with open('commute_time.json', 'w') as file:
+        json.dump(commute_time, file, indent=4)
+
+
+if __name__ == '__main__':
+    get_commute_time()
